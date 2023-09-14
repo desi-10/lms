ef38fc3e4086e2a35df4718a2983fdc4220b3752
King-fReeBoDy <89941338+King-fReeBoDy@users.noreply.github.com>
2023-09-14 12:41:36 +0000

    login update

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,12 @@
 const Login = () => {
   return (
     <main className="h-screen flex justify-center items-center bg-slate-50 text-slate-500">
-      <div>
+      <div className="w-[90%] mx-auto  md:w-[500px] lg:w-[1000px] py-10">
         <h2 className="hidden text-2xl lg:block my-4 text-center font-bold text-black">
           Learning Management System
         </h2>
-        <section className="sm:w-full mx-auto grid grid-cols-1 lg:grid-cols-2 md:w-[500px] lg:w-[1000px] justify-center items-center">
-          <div className="w-full md:w-[300px] lg:w-full block mx-auto">
+        <section className="mx-auto grid grid-cols-1 lg:grid-cols-2 justify-center items-center">
+          <div className="w-[200px] md:w-[300px] lg:w-full block mx-auto">
             <img src="/assets/blogging-clipart.svg" alt="" />
           </div>
 
@@ -19,8 +19,9 @@ const Login = () => {
                 Index number
               </label>
               <input
-                type="number"
+                type="text"
                 name=""
+                maxLength={10}
                 className="p-2 border rounded-lg bg-slate-50"
               />
             </div>
@@ -31,6 +32,7 @@ const Login = () => {
               <input
                 type="password"
                 name=""
+                maxLength={20}
                 className="p-2 border rounded-lg bg-slate-50"
               />
             </div>
@@ -48,10 +50,12 @@ const Login = () => {
             </button>
 
             <div className="flex justify-between items-center">
-              <p className="text-sm underline text-blue-700">
+              <p className="text-sm underline text-blue-700 cursor-pointer">
                 Forgetten Password
               </p>
-              <p className="py-2 px-5 border rounded-lg text-sm">I need help</p>
+              <p className="py-2 px-5 border rounded-lg text-sm cursor-pointer">
+                I need help
+              </p>
             </div>
           </form>
         </section>
