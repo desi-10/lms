81ab4c721a36cadcde19c25c627256afade8b5c1
MatrixMe00 <safosah00@gmail.com>
2023-09-27 17:49:52 +0000

    Update Student.php
    Started progress on login section
    Created a function for creating an index number

--- app/Student.php
+++ app/Student.php
@@ -15,20 +15,38 @@
             $this->index_number = $value;
         }
 
-        public function login() :int|bool{
+        public function login() :int|string|bool{
             $response = false;
 
             list("index_number" => $index_number, "password" => $password) = $_POST;
 
             //search the index number
-            $found_index = $this->connect->fetch("user_id","students","index_number='$index_number'");
-
-            if($found_index){
-                
+            $found_index = self::$connect->fetch("user_id","students",
+                "index_number='$index_number'", no_results:"Student with index number '$index_number' not found");
+            
+            if($found_index === true){
+                $response = true;
+            }elseif($found_index !== false){
+                $response = $found_index;
             }else{
                 $response = false;
             }
 
             return $response;
+        }
+        
+        public function create(array $details) :bool|string{
+            $response = true;
+
+            return $response;
+        }
+
+        public function createIndexNumber(){
+            $department_id = "08";
+            $indexNumber = "03" . date("y") . $department_id;
+            $unique = rand(1,999);
+            $indexNumber .= str_pad((string) $unique, 4, "0", STR_PAD_LEFT);
+
+            return $indexNumber;
         }
     }
