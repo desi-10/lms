528f553d523300d6c97f855c203554e31d5a02c8
MatrixMe00 <safosah00@gmail.com>
2023-09-27 17:50:38 +0000

    Update index.php
    making tests of user data entry and searches

--- public/index.php
+++ public/index.php
@@ -2,14 +2,24 @@
     require_once "../autoloader.php";
 
     use App\Student;
+    use App\Database;
 
     $_POST["index_number"] = "0123456789";
     $_POST["password"] = "password";
 
-    // if(isset($_POST["login"])){
+    // if(isset($_POST["submit"])){
         $student = new Student();
+        $db = new Database;
+        $users = [
+            "username" => "MatrixMe",
+            "lname" => "Afosah",
+            "oname" => "Seth Boye",
+            "user_role" => 1
+        ];
     // }
     
     echo "<pre>";
-    var_dump($student->login());
+    var_dump($student->login(), $student->createIndexNumber());
+    // var_dump($db->insert("users",$users), $db->getLogs(), $db->status());
+    // var_dump($db->fetch("id","users"), $db->getLogs());
     echo "</pre>";
