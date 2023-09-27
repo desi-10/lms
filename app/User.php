1f4b38239483fa1dc37e206055d2997124906ddc
MatrixMe00 <safosah00@gmail.com>
2023-09-27 17:50:19 +0000

    Update User.php
    Started working on the create feature

--- app/User.php
+++ app/User.php
@@ -4,7 +4,7 @@
 
     class User
     {
-        protected Database $connect;
+        protected static Database $connect;
         
         public function __construct(
             protected int $user_id = 0, protected string $lname = "", protected string $oname = "",
@@ -16,12 +16,18 @@
             $this->username = $username;
             $this->user_role = $user_role;
 
-            $this->connect = new Database;
+            self::$connect = new Database;
         }
 
-        public function login() :int|bool{
+        public function login() :int|string|bool{
             $response = false;
 
+            return $response;
+        }
+
+        public function create(array $details) :bool|string{
+            $response = $this->connect->insert("users", $details);
+            
             return $response;
         }
     }
