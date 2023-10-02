ecc72e4d40090da1a206840c09d9b1b4e83673c3
MatrixMe00 <safosah00@gmail.com>
2023-10-02 11:25:59 +0000

    Remaking directory structure

--- src/api/autoload.php
+++ src/api/autoload.php
@@ -1,5 +1,6 @@
 <?php 
+    
     spl_autoload_register(function($class){
         // $class_name = explode("\\", $class)[1];
-        require_once "$class.php";
+        require_once "../../$class.php";
     });
--- src/api/index.php
+++ src/api/index.php
@@ -1,5 +1,5 @@
 <?php 
-    require_once "../autoloader.php";
+    require_once "../../autoloader.php";
 
     use App\Student;
 
