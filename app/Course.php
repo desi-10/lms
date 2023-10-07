a69d4aecf4a9d733bbc51b8247b6be0102c1af29
MatrixMe00 <safosah00@gmail.com>
2023-10-07 14:36:43 +0000

    Update Course.php
    Started making changes to course class

--- app/Course.php
+++ app/Course.php
@@ -8,6 +8,13 @@
         private string $course_name;
         private string $course_code;
 
+        public function __construct(private Database $connect, int $course_id = 0, string $course_name = "", string $course_code){
+            $this->connect = $connect;
+            $this->course_code = $course_code;
+            $this->course_name = $course_name;
+            $this->course_id = $course_id;
+        }
+
         public function add() :bool{
             $success = false;
             
