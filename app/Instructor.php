dcf1081fc6df79c948284b20dcb27cf502cfd532
MatrixMe00 <safosah00@gmail.com>
2023-10-10 11:35:05 +0000

    Update Instructor.php
    View all courses of an instructor

--- app/Instructor.php
+++ app/Instructor.php
@@ -10,4 +10,17 @@
             string $oname = '', string $username = '', int $user_role = 2){
                 parent::__construct($database, $user_id, $lname, $oname, $username, $user_role);
         }
+
+        /**
+         * Returns all courses for the instructor
+         * @return array|bool A list of courses or a bool data
+         */
+        public function courses() :array|string|bool{
+            $response = false;
+
+            $response = new Course(static::$connect, instructor_id: $this->user_id);
+            $response = $response->all();
+
+            return $response;
+        }
     }
