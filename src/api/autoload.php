2a845856ab561644b5075b3cc47e7f43eadb0940
MatrixMe00 <safosah00@gmail.com>
2023-10-25 14:19:43 +0000

    Live Server compatibility update

--- src/api/autoload.php
+++ src/api/autoload.php
@@ -1,8 +1,9 @@
 <?php 
     
     spl_autoload_register(function($class){
-        $class = str_replace("\\",DIRECTORY_SEPARATOR, $class);
+        $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
         $class = str_replace("App","app", $class);
         $class = str_replace("Trait","trait", $class);
+        $class = str_replace("Controller/","controller/", $class);
         require_once "../../$class.php";
     });
